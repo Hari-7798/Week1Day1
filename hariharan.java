0b756156b0a4555a267a2497346778dd4e118f3a
Hari-7798 <88588726+Hari-7798@users.noreply.github.com>
2021-08-07 20:41:31 +0530

    Add files via upload

---
+++ hariharan.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class hariharan {
+	public static void main(String[] args) {
+		System.out.println("All is going well");
+
+	}
+
+}
---
+++ mymobile.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class mymobile {
+	public static void main(String[] args) {
+		String brandName= "POCOF1";
+		
+		char colour= 'B';
+		int price= 20000;
+		byte memory= 64;
+		float screensize= 6.2f;
+		boolean isTouchscreen= true;
+		long phonenumber= 8248277905L;
+		
+		System.out.println("BrandName"+brandName);
+		System.out.println("Colour"+colour);
+		System.out.println("Price"+price);
+		System.out.println("Memory"+memory);
+		System.out.println("ScreenSize"+screensize);
+		System.out.println("ScreenType"+isTouchscreen);
+		System.out.println("Mobilenumber"+phonenumber);
+
+	}
+}
